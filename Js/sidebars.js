398e265671cfe7e4e29d438cfcab70f783335e8b
hafizzahabdulwahib <155596531+hafizzahabdulwahib@users.noreply.github.com>
2025-02-06 09:52:48 +0800

    Create sidebars.js
    first commit

---
+++ Js/sidebars.js
@@ -0,0 +1,9 @@
+module.exports = {
+  docs: [
+    {
+      type: 'doc',
+      id: 'my-name', // Match the filename (without .mdx)
+      label: 'My Name', // Display name in sidebar
+    },
+  ],
+};
